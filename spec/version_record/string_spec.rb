e5def66133c13fcb81fbdb76ab38d74b258f940b
Marcelo Casiraghi <marcelocasiraghi@gmail.com>
2018-01-08 15:32:06 -0300

    test: add test on String#to_version

--- spec/version_record/string_spec.rb
+++ spec/version_record/string_spec.rb
@@ -1,6 +1,7 @@
 describe String do
   describe '#to_version' do
     it { expect('2.2.1'.to_version).to eq VersionRecord::Version.new('2.2.1') }
+    it { expect('2.2.1.beta'.to_version).to eq VersionRecord::Version.new('2.2.1.beta') }
     it { expect { 'invalid_version'.to_version }.to raise_error(ArgumentError) }
   end
 end
